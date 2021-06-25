8863feaa3b4b9b865370d65cdf43abb03aed95fc
EwaGrazka <fk.grazka@gamil.com>
2021-06-25 21:19:10 +0200

    userList fetch + styling

--- users-list/src/MainView/MainView.jsx
+++ users-list/src/MainView/MainView.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MainView.scss';
 
-
 const MainView = () => {
-  return <div className="main-view-container">Main View</div>
+  const [usersList, setUsersList] = useState(null)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsersList(data))
+  }, [setUsersList]);
+
+  return (
+    <div className="main-view-container">
+      <h1>Users list</h1>
+      <input placeholder="Search by user name" />
+      <ol>
+        {usersList?.map((user) => (
+          <li><span className="user-name">{user.name}</span><span className="user-username">{`@${user.username}`}</span></li>
+        ))}
+      </ol>
+    </div>
+  )
 };
 
 export default MainView;
