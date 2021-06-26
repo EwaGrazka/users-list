1f9aee509583abf321159584234f6ddd7fce580d
EwaGrazka <fk.grazka@gamil.com>
2021-06-26 08:05:21 +0200

    search input (dynamic search by name)

--- users-list/src/MainView/MainView.jsx
+++ users-list/src/MainView/MainView.jsx
@@ -2,25 +2,58 @@ import React, { useEffect, useState } from 'react';
 import './MainView.scss';
 
 const MainView = () => {
-  const [usersList, setUsersList] = useState(null)
+  const [users, setusers] = useState([])
+  const [currentUsers, setcurrentUsers] = useState([]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(data => setUsersList(data))
-  }, [setUsersList]);
+      .then(data => setusers(data))
+  }, []);
 
+  useEffect(() => {
+    setcurrentUsers(users)
+  }, [users]);
+
+  const search = (e) => {
+    const text = e.target.value;
+    let usersFound = users;
+
+    if (text) {
+      usersFound = users.filter((user) => {
+        return (
+          user.name.toLowerCase().search(text.toLowerCase()) > -1
+        );
+      });
+    }
+
+    setcurrentUsers(usersFound);
+  };
+
+
+  const usersToRender = currentUsers.map((row) => row)
+
+  const searchedUsers =
+    usersToRender.length > 0 ? (
+      usersToRender.map((userName) => <li><span className="user-name">{userName.name}</span> <span className="user-username">{`@${userName.username}`}</span></li>)
+
+    ) : (
+      <p>No results. Please, try again.</p>
+    );
   return (
     <div className="main-view-container">
       <h1>Users list</h1>
-      <input placeholder="Search by user name" />
+      <input type="search" placeholder="Search by user name" onChange={search} />
       <ol>
-        {usersList?.map((user) => (
-          <li><span className="user-name">{user.name}</span><span className="user-username">{`@${user.username}`}</span></li>
-        ))}
+        {searchedUsers}
       </ol>
     </div>
   )
 };
 
 export default MainView;
+
+
+
+
+
